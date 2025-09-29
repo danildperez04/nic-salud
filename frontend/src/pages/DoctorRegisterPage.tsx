fff521a3c2b6374857cb37134a5d3c00c91270bc
danildperez04 <gamerdan589@gmail.com>
2025-09-29 07:59:27 -0600

    feat: add register form

---
+++ frontend/src/pages/DoctorRegisterPage.tsx
@@ -0,0 +1,10 @@
+import { DoctorRegisterForm } from '@/components/DoctorRegisterForm'
+
+export function DoctorRegisterPage() {
+  return (
+    <div className="max-w-md mx-auto">
+      <h2 className="text-xl font-bold mb-4">Registro de Doctor</h2>
+      <DoctorRegisterForm />
+    </div>
+  )
+}
